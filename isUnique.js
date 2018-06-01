86ee1bee25b8779a0e4136d82d68c9e4990e8c20
gkolb <gkolb100@gmail.com>
2018-06-01 09:27:15 -0600

    Add new solution for isUnique

--- isUnique.js
+++ isUnique.js
@@ -1,14 +1,23 @@
-var isUnique = function(string) {
-  var charObject = {}
+/* first guess
+  var isUnique = function(string) {
+    var charObject = {}
+    for (var i = 0; i < string.length; i++) {
+        if (charObject[string[i]]) {
+            return false;
+        }
+        charObject[string[i]] = true;
+    }
+    return true;
+  }
+*/
+
+export function isUnique(string) {
+  var char = new Set();
   for (var i = 0; i < string.length; i++) {
-      if (charObject[string[i]]) {
-          return false;
-      }
-      charObject[string[i]] = true;
+    if (char.has(string[i])) {
+      return false;
+    }
+    char.add(string[i]);
   }
   return true;
 }
-
-console.log(isUnique('hey'))
-console.log(isUnique('heyy'))
-console.log(isUnique('greg'))
